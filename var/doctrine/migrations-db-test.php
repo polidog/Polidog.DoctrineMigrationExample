3af7145fb74e0908b1f73642d4936cb4990d12a6
polidog <polidogs@gmail.com>
2024-03-13 00:42:40 +0900

    Fix dotenv file directory.

--- var/doctrine/migrations-db-test.php
+++ var/doctrine/migrations-db-test.php
@@ -4,7 +4,7 @@ use BEAR\Dotenv\Dotenv;
 
 require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
-(new Dotenv())->load(__DIR__);
+(new Dotenv())->load(dirname(__DIR__, 2));
 
 return [
     'dbname' => getenv('DB_NAME') . '_test',
