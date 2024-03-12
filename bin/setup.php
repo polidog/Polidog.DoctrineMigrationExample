35a684dd27affdbf896d8a14f46a37753fcfb898
polidog <polidogs@gmail.com>
2024-03-13 01:02:21 +0900

    Add setup.php

--- bin/setup.php
+++ bin/setup.php
@@ -2,5 +2,22 @@
 
 declare(strict_types=1);
 
+use BEAR\Dotenv\Dotenv;
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+(new Dotenv())->load(dirname(__DIR__));
+
 chdir(dirname(__DIR__));
 passthru('rm -rf ./var/tmp/*');
+
+// データベースの作成
+$pdo = new \PDO(getenv('DB_DSN'), getenv('DB_USER'), getenv('DB_PASSWORD'));
+$pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('DB_NAME'));
+$pdo->exec('DROP DATABASE IF EXISTS ' . getenv('DB_NAME') . '_test');
+$pdo->exec('CREATE DATABASE ' . getenv('DB_NAME') . '_test');
+
+// migrationの実行
+$migrationsConfigPath = dirname(__DIR__) . '/var/doctrine/migrations.php';
+$dbConfigPath = dirname(__DIR__) . '/var/doctrine/migrations-db.php';
+$dbConfigTestPath = dirname(__DIR__) . '/var/doctrine/migrations-db-test.php';
+passthru("vendor/bin/doctrine-migrations migrations:migrate --configuration=$migrationsConfigPath --db-configuration=$dbConfigPath --no-interaction ");
+passthru("vendor/bin/doctrine-migrations migrations:migrate --configuration=$migrationsConfigPath --db-configuration=$dbConfigTestPath --no-interaction ");
