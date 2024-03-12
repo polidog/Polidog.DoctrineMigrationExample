5e8c570c4fe6165d4bef9dbfd4e1b26f0a45a221
polidog <polidogs@gmail.com>
2024-03-13 00:33:00 +0900

    Add db connection file

---
+++ var/doctrine/migrations-db-test.php
@@ -0,0 +1,16 @@
+<?php
+
+use BEAR\Dotenv\Dotenv;
+
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+
+(new Dotenv())->load(__DIR__);
+
+return [
+    'dbname' => getenv('DB_NAME') . '_test',
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'host' => getenv('DB_HOST'),
+    'port' => getenv('DB_PORT'),
+    'driver' => getenv('DB_DRIVER')
+];
